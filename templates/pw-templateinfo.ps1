97f206eaa25209d782c20d10d707cf8eaa38f085
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-04-10 12:55:59 -0700

    adding templates

---
+++ templates/pw-templateinfo.ps1
@@ -0,0 +1,50 @@
+﻿[cmdletbinding()]
+param()
+
+$templateInfo = New-Object -TypeName psobject -Property @{
+    Name = 'appveyor.ps1'
+    Type = 'ItemTemplate'
+}
+$templateInfo | add-sourcefile -sourceFiles 'appveyor.ps1'
+Set-TemplateInfo -templateInfo $templateInfo
+
+$templateInfo = New-Object -TypeName psobject -Property @{
+    Name = 'appveyor.yml'
+    Type = 'ItemTemplate'
+}
+$templateInfo | add-sourcefile -sourceFiles 'appveyor.yml'
+Set-TemplateInfo -templateInfo $templateInfo
+
+$templateInfo = New-Object -TypeName psobject -Property @{
+    Name = 'build.ps1'
+    Type = 'ItemTemplate'
+}
+$templateInfo | add-sourcefile -sourceFiles 'build.ps1'
+Set-TemplateInfo -templateInfo $templateInfo
+
+$templateInfo = New-Object -TypeName psobject -Property @{
+    Name = '.gitattributes'
+    Type = 'ItemTemplate'
+}
+$templateInfo | add-sourcefile -sourceFiles 'gitattributes.txt' -destFiles {'.gitattributes'}
+Set-TemplateInfo -templateInfo $templateInfo
+
+$templateInfo = New-Object -TypeName psobject -Property @{
+    Name = 'nuspec'
+    Type = 'ItemTemplate'
+}
+$templateInfo | add-sourcefile -sourceFiles 'sample.nuspec' -destFiles {"$ItemName.nuspec"}
+Set-TemplateInfo -templateInfo $templateInfo
+
+
+
+
+
+
+
+
+
+<#
+$templateInfo | update-filename (
+    ,('controller.js', {"$ItemName.js"})
+)#>
