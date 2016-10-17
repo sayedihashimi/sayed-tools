4aadf6abea3144cc7ff65d0bb930420cd8e04ef8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-10-17 15:54:54 -0700

    adding githubps

---
+++ powershell/github-ps.psm1
@@ -0,0 +1,40 @@
+﻿[cmdletbinding()]
+param()
+
+function CloneRepo{
+    [cmdletbinding()]
+    param(
+        [ValidateNotNullOrEmpty()]
+        [string]$user,
+
+        [ValidateNotNullOrEmpty()]
+        [string]$repo,
+
+        [ValidateSet('ssh','https')]
+        [string]$method = 'ssh'
+    )
+    process{
+        # ssh pattern git@github.com:sayedihashimi/aspnettemplates.git
+        # https pattern https://github.com/sayedihashimi/aspnettemplates.git
+
+        switch ($method){
+            'ssh' {
+                $url = ('git@github.com:{0}/{1}.git' -f $user, $repo)
+            }
+
+            'https' {
+                $url = ('https://github.com/{0}/{1}.git' -f $user, $repo)
+            }
+
+            'default' {
+                throw ('Unknown value for clone method [{0}]' -f $method)
+            }
+        }
+
+        & cmd.exe /C "git clone $url 2>&1"
+        $gitexitcode = $LASTEXITCODE
+        if($LASTEXITCODE -ne 0){
+            throw $Error[0]
+        }
+    }
+}
