2e6ec9ab67ad4ad2037313c9b6d4868850ce0f46
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-11-12 20:11:50 -0800

    adding update-wallpaper

--- powershell/sayed-tools.psm1
+++ powershell/sayed-tools.psm1
@@ -348,6 +348,73 @@ function Open-StartupFolder{
     }
 }
 
+# taken from http://www.theagreeablecow.com/2014/09/set-desktop-wallpaper-using-powershell.html
+Function Update-Wallpaper {
+    Param(
+        [Parameter(Mandatory=$true)]
+        $Path,
+         
+        [ValidateSet('Center','Stretch','Fill','Tile','Fit')]
+        $Style = 'Fit'
+    )
+    if(-not (test-path -Path $Path -PathType Leaf)){
+        'File not found at [{0}]' -f $Path | Write-error
+        break
+    }
+    Try {
+        if (-not ([System.Management.Automation.PSTypeName]'Wallpaper.Setter').Type) {
+            Add-Type -TypeDefinition @"
+            using System;
+            using System.Runtime.InteropServices;
+            using Microsoft.Win32;
+            namespace Wallpaper {
+                public enum Style : int {
+                    Center, Stretch, Fill, Fit, Tile
+                }
+                public class Setter {
+                    public const int SetDesktopWallpaper = 20;
+                    public const int UpdateIniFile = 0x01;
+                    public const int SendWinIniChange = 0x02;
+                    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+                    private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+                    public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+                        SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+                        RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+                        switch( style ) {
+                            case Style.Tile :
+                                key.SetValue(@"WallpaperStyle", "0") ; 
+                                key.SetValue(@"TileWallpaper", "1") ; 
+                                break;
+                            case Style.Center :
+                                key.SetValue(@"WallpaperStyle", "0") ; 
+                                key.SetValue(@"TileWallpaper", "0") ; 
+                                break;
+                            case Style.Stretch :
+                                key.SetValue(@"WallpaperStyle", "2") ; 
+                                key.SetValue(@"TileWallpaper", "0") ;
+                                break;
+                            case Style.Fill :
+                                key.SetValue(@"WallpaperStyle", "10") ; 
+                                key.SetValue(@"TileWallpaper", "0") ; 
+                                break;
+                            case Style.Fit :
+                                key.SetValue(@"WallpaperStyle", "6") ; 
+                                key.SetValue(@"TileWallpaper", "0") ; 
+                                break;
+}
+                        key.Close();
+                    }
+                }
+            }
+"@ -ErrorAction Stop 
+            } 
+        } 
+        Catch {
+            Write-Warning -Message "Wallpaper not changed because $($_.Exception.Message)"
+        }
+    [Wallpaper.Setter]::SetWallpaper( $Path, $Style )
+}
+
 <#
 .SYNOPSIS 
 This will inspect the nuspec file and return the value for the Version element.
