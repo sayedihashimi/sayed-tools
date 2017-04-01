31a63fda7c13947b33533af72d949702b91997af
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-04-01 14:56:33 -0700

    updating dotnet help script

--- powershell/dotnet/gen-dotnet-helpoutput.ps1
+++ powershell/dotnet/gen-dotnet-helpoutput.ps1
@@ -11,15 +11,41 @@ if([string]::IsNullOrWhiteSpace($outputPath)) {
 
 [object[]]$commandsToRun = @(
     @{'command'='dotnet'},
+    @{'command'='dotnet';'Cargs'=@('--version')},
     @{'command'='dotnet';'Cargs'=@('-h')},
+
+    @{'command'='dotnet';'CArgs'=@('build','-h')},
+    @{'command'='dotnet';'CArgs'=@('clean','-h')},
+    @{'command'='dotnet';'CArgs'=@('restore','-h')}
+    @{'command'='dotnet';'CArgs'=@('publish','-h')},
+    @{'command'='dotnet';'CArgs'=@('test','-h')},
+    @{'command'='dotnet';'CArgs'=@('pack','-h')},
+    @{'command'='dotnet';'CArgs'=@('migrate','-h')},
+
     @{'command'='dotnet';'Cargs'=@('new')},
     @{'command'='dotnet';'CArgs'=@('new','-h')},
     @{'command'='dotnet';'CArgs'=@('new','-l')},
+    @{'command'='dotnet';'CArgs'=@('new','classlib','-h')}
+    @{'command'='dotnet';'CArgs'=@('new','mstest','-h')}
+    @{'command'='dotnet';'CArgs'=@('new','xunit','-h')}
     @{'command'='dotnet';'CArgs'=@('new','web','-h')},
     @{'command'='dotnet';'CArgs'=@('new','mvc','-h')}
+    @{'command'='dotnet';'CArgs'=@('new','webapi','-h')}
+    @{'command'='dotnet';'CArgs'=@('new','sln','-h')}
+
+    @{'command'='dotnet-new3'},
+    @{'command'='dotnet-new3';'CArgs'=@('-h')},
+    @{'command'='dotnet-new3';'CArgs'=@('-l')},
+    @{'command'='dotnet-new3';'CArgs'=@('console','-h')}
+    @{'command'='dotnet-new3';'CArgs'=@('classlib','-h')}
+    @{'command'='dotnet-new3';'CArgs'=@('mstest','-h')}
+    @{'command'='dotnet-new3';'CArgs'=@('xunit','-h')}
+    @{'command'='dotnet-new3';'CArgs'=@('web','-h')},
+    @{'command'='dotnet-new3';'CArgs'=@('mvc','-h')}
+    @{'command'='dotnet-new3';'CArgs'=@('webapi','-h')}
+    @{'command'='dotnet-new3';'CArgs'=@('sln','-h')}
 )
 
-# $commands | % { & ($_.command) ($_.CArgs) }
 
 [int]$runDnCommandIndex = 0
 function RunDnCommand{
@@ -35,8 +61,13 @@ function RunDnCommand{
         $runDnCommandIndex++
         foreach($dnCommand in $command){
             if($dnCommand -ne $null){
-                $actualCommand = ($dnCommand.command)
-                
+                $actualCommand = ($dnCommand.command).Trim()
+                if( ('dotnet-new3'.Equals($actualCommand)) -and (-not(get-alias 'dotnet-new3')) ){
+                    'dotnet-new3 not found skipping' | Write-Verbose
+                    continue
+                }
+
+
                 if(-not ([string]::IsNullOrWhiteSpace($actualCommand))){
                     $cmdArgs = ($dnCommand.CArgs)
                     #[string]$cmdText = ($actualCommand, ( $cmdArgs -join ' ' ))
@@ -67,5 +98,11 @@ pwd: [{0}]
 $outputPath [{1}]
 '@ -f $pwd,$outputPath | Write-Host -ForegroundColor Yellow
 
+if(-not ([string]::IsNullOrWhiteSpace($outputPath))){
+    Get-ChildItem $outputPath -File | Remove-Item
 
-$commandsToRun | RunDnCommand -outputFolder $outputPath
+    $commandsToRun | RunDnCommand -outputFolder $outputPath
+}
+else{
+    'outputpath was empty' | Write-Error
+}
