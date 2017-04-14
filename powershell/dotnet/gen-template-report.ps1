d38c706f2c9e46ad1507f2071f5a8b6801023067
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-04-14 14:06:53 -0700

    updating gen-template and appveyor.yml

--- powershell/dotnet/gen-template-report.ps1
+++ powershell/dotnet/gen-template-report.ps1
@@ -1,5 +1,91 @@
 [cmdletbinding()]
-param()
+param(
+    [Parameter(Position=0)]
+    [string]$searchTerm = 'template'
+)
+
+$global:machinesetupconfig = @{
+    MachineSetupConfigFolder = (Join-Path $env:temp 'SayedHaMachineSetup')
+    MachineSetupAppsFolder = (Join-Path $env:temp 'SayedHaMachineSetup\apps')
+    RemoteFiles = (join-path $env:temp 'SayedHaMachineSetup\remotefiles')
+}
+
+function Get7ZipPath{
+    [cmdletbinding()]
+    param()
+    process{
+        (join-path $env:ProgramFiles '7-Zip\7z.exe')
+    }
+}
+
+function EnsureFolderExists{
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0,ValueFromPipeline=$true)]
+        [string[]]$path
+    )
+    process{
+        foreach($p in $path){
+            if(-not [string]::IsNullOrWhiteSpace($p) -and (-not (Test-Path $p))){
+                New-Item -Path $p -ItemType Directory
+            }
+        }
+    }
+}
+
+function GetLocalFileFor{
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$downloadUrl,
+
+        [Parameter(Position=1,Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$filename
+    )
+    process{
+        $expectedPath = (Join-Path $global:machinesetupconfig.RemoteFiles $filename)
+        
+        if(-not (test-path $expectedPath)){
+            # download the file
+            EnsureFolderExists -path ([System.IO.Path]::GetDirectoryName($expectedPath)) | out-null            
+            Invoke-WebRequest -Uri $downloadUrl -OutFile $expectedPath | out-null
+        }
+
+        if(-not (test-path $expectedPath)){
+            throw ('Unable to download file from [{0}] to [{1}]' -f $downloadUrl, $expectedPath)
+        }
+
+        $expectedPath
+    }
+}
+
+function ExtractRemoteZip{
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$downloadUrl,
+
+        [Parameter(Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [string]$filename
+    )
+    process{
+        $zippath = GetLocalFileFor -downloadUrl $downloadUrl -filename $filename
+        $expectedFolderpath = (join-path -Path ($global:machinesetupconfig.MachineSetupConfigFolder) ('apps\{0}\' -f $filename))
+
+        if(-not (test-path $expectedFolderpath)){
+            EnsureFolderExists -path $expectedFolderpath | Write-Verbose
+            # extract the folder to the directory
+            & (Get7ZipPath) x -y "-o$expectedFolderpath" "$zippath" | Write-Verbose
+        }        
+
+        # return the path to the folder
+        $expectedFolderpath
+    }
+}
 
 function GetTemplatesToCheck(){
     [cmdletbinding()]
@@ -30,22 +116,25 @@ function GetTemplatesToCheck(){
 function Get-PackageDownloadStats(){
     [cmdletbinding()]
     param(
-        [string[]]$packageName,
+        [Parameter(Position=0,ValueFromPipeline=$true)]
+        [object[]]$package,
         [string]$urlformat = 'http://www.nuget.org/packages/{0}/'
     )
     process{
         # $html = (Invoke-WebRequest -Uri 'http://www.nuget.org/packages/SlowCheetah/').rawcontent
-        foreach($pkg in $packageName){
+        foreach($pkgobj in $package){
+            [string]$pkgname = $pkgobj.Name
             [int]$dlcount = -1;
-            [string]$url = ($urlformat -f $pkg)
-            [string]$html = ((Invoke-WebRequest -Uri $url -ErrorAction SilentlyContinue).rawcontent)
+            [string]$packageurl = ($urlformat -f $pkgname)
+            [string]$html = ((Invoke-WebRequest -Uri $packageurl -ErrorAction SilentlyContinue).rawcontent)
             if(-not([string]::IsNullOrWhiteSpace($html))) {
                 $htmllines = $html.split("`n")
                 $dlstring = (((( $htmllines|Select-String '<p class="stat-label">Downloads</p>' -SimpleMatch -Context 1))) | Select-Object -ExpandProperty Context | Select-Object -ExpandProperty PreContext)
                 if($dlstring -match '<p class="stat-number">([0-9,]+)<\/p>'){
                     $dlcount = ($Matches[1])
                 }
-
+                $downloadUrl = $pkgobj.DownloadUrl
+                <#    
                 [string]$downloadUrl = $null
                 try{
                     $downloadUrl = ( $htmllines|Select-String '<a href="([^\"]+)" title=\"Download the raw nupkg file."').Matches.Groups[1].value                    
@@ -53,12 +142,55 @@ function Get-PackageDownloadStats(){
                 catch{
                     $downloadUrl = $null
                 }
+                #>
             }
 
             New-Object -TypeName psobject -Property @{
-                'Name'=$pkg
+                'Name'=$pkgname
                 'DownloadCount'=$dlcount
                 'Downloadurl'=$downloadUrl
+                'Version'=$pkgobj.Version
+            }
+        }
+    }
+}
+
+function Test-PathContainsTemplate(){
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=0)]
+        [string]$pathToCheck
+    )
+    process{
+        if(test-path $pathToCheck){
+            # if the folder contains a template return the path, otherwise return nothing.
+            [string[]]$templateFiles = (Get-ChildItem $pathToCheck .template.config -Directory -Recurse|%{Get-ChildItem (get-item ($_).fullname) template.json -File})
+            if( ($templateFiles -ne $null) -and ($templateFiles.Length -gt 0)){
+
+                # return the folder path
+                $true
+            }
+            else{
+                $false
+            }
+        }
+    }
+}
+
+function Find-PathContainingTemplate(){
+    [cmdletbinding()]
+    param(
+        [Parameter(Position=1,ValueFromPipeline)]
+        [string[]]$pathToCheck
+    )
+    process{
+        foreach($folderpath in $pathToCheck){
+            # if the folder contains a template return the path, otherwise return nothing.
+            $templateFiles = (Get-ChildItem $path .template.config -Directory -Recurse|%{Get-ChildItem (get-item ($_).fullname) template.json -File})
+            if($templateFiles -ne $null -and ($templateFiles.Length -gt 0)){
+
+                # return the folder path
+                $folderpath
             }
         }
     }
@@ -70,17 +202,25 @@ function Get-TemplateReport{
         [string]$searchTerm = 'template'       
     )
     process{
-        #$templates = GetTemplatesToCheck -searchTerm $searchTerm
-
+        # list of templates to check
         $pkgs = (GetTemplatesToCheck -searchTerm $searchTerm |
                     ForEach-Object { 
-                        Get-PackageDownloadStats -packageName $_ })
+                        Get-PackageDownloadStats -package $_ })
+        
+        [object[]]$foundTemplatePackages = @()
+        [string[]]$pathsToCheck = @()
+        # download packages locally and get path to installed location
+        foreach($pkg in $pkgs){
+            $filename = '{0}.{1}.nupkg' -f $pkg.Name,$pkg.Version
+            $extractpath = ExtractRemoteZip -downloadUrl $pkg.DownloadUrl -filename $filename
+            'extractpath: {0}' -f $extractpath | Write-Verbose
+            # $pathsToCheck += $extractpath
+            if( (Test-PathContainsTemplate -pathToCheck $extractpath) -eq $true){
+                $pkg
+            }
+        }
 
-
-
-
-
-        # download each template
+        # $foundTemplatePackages
     } 
 }
 
@@ -122,8 +262,10 @@ function Get-Nuget{
 
 
 try{
+    $global:foundpackages = @()
+    $global:foundpackages += ( Get-TemplateReport )
 
-    Get-Nuget
+    $global:foundpackages
 
 }
 catch{
