3554d6eee49f00ea8a7157f497b60d65eece6bed
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-04-19 10:49:38 -0700

    minor update

--- powershell/dotnet/gen-template-report.ps1
+++ powershell/dotnet/gen-template-report.ps1
@@ -1,7 +1,7 @@
 [cmdletbinding()]
 param(
     [Parameter(Position=0)]
-    [string[]]$searchTerm = @('template','templates','KenticoCloud.CloudBoilerplateNet'),
+    [string[]]$searchTerm = @('template','templates','KenticoCloud.CloudBoilerplateNet','ServiceStack.Core.Templates'),
     
     [Parameter(Position=1)]
     [switch]$skipReport,
