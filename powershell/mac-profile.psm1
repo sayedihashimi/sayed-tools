bc0b0786a8db2aeb5a209ee4db0490fdbb43dbb8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-05-02 16:27:55 -0700

    updating macprofile

--- powershell/mac-profile.psm1
+++ powershell/mac-profile.psm1
@@ -1,10 +1,65 @@
 [cmdletbinding()]
 param()
 
-[string[]]$modsToImport = @(
-    (Join-Path $Global:codehome 'sayed-tools/powershell/github-ps.psm1')
-    (Join-Path $Global:codehome 'sayed-tools/powershell/sayed-tools.psm1')
+$global:MacProfileSettings = New-Object PSObject -Property @{
+    HomeDir = $env:HOME
+    ModulesToLoad = @(
+        (get-item (join-path -Path $global:codehome 'sayed-tools/powershell/github-ps.psm1' -ErrorAction SilentlyContinue)).FullName
+        (get-item (join-path -Path $global:codehome 'sayed-tools/powershell/sayed-tools.psm1' -ErrorAction SilentlyContinue)).FullName
+        (get-item (join-path $Global:dropboxpath 'Personal/PcSettings/Powershell/sayed-profile-xplat.psm1' -ErrorAction SilentlyContinue)).FullName        
+    )
+}
+
+function Set-InitialPath{
+    [cmdletbinding()]
+    param()
+    process{
+        if(test-path $global:codehome){
+            Push-Location
+            Set-Location -Path $global:codehome
+        }
+    }
+}
+
+function Import-MyModules{
+    [cmdletbinding()]
+    param()
+    process{
+        [bool]$allImported = $true
+        foreach ($modpath in $global:MacProfileSettings.ModulesToLoad) {
+            if(Test-Path $modpath){
+                'Importing module from [{0}]' -f $modpath | Write-Host
+                Import-Module $modpath -Global -DisableNameChecking | Write-Verbose
+            }
+            else{
+                $allImported = $false
+                'Module not found at [{0}]' -f $modpath | Write-Output
+            }
+        }
+
+        # return true if all imported
+        $allImported
+    }
+}
+
+<#
+[string[]]$modsToLoad = @(
+    (join-path -Path $global:codehome 'sayed-tools/powershell/github-ps.psm1' -ErrorAction SilentlyContinue)
+    (join-path -Path $global:codehome 'sayed-tools/powershell/sayed-tools.psm1' -ErrorAction SilentlyContinue)
+    (join-path $Global:dropboxpath 'Personal/PcSettings/Powershell/sayed-profile-xplat.psm1' -ErrorAction SilentlyContinue)
+    # (join-path $Global:dropboxpath 'Personal/PcSettings/Powershell/sayed-profile.psm1' -ErrorAction SilentlyContinue)
 )
+
+foreach ($modpath in $modsToLoad) {
+    if(Test-Path $modpath){
+        'Importing module from [{0}]' -f $modpath | Write-Host
+        Import-Module $modpath -Global -DisableNameChecking
+    }
+    else{
+        'Module not found at [{0}]' -f $modpath | Write-Output
+    }
+}
+#>
 
 function clip{
     [cmdletbinding()]
@@ -17,4 +72,26 @@ function clip{
             $inObj | pbcopy
         }
     }
+}
+
+function Ensure-GitConfigExists{
+    [cmdletbinding()]
+    param(
+        [string]$gitconfigpath = ([System.IO.Path]::GetFullPath((join-path $global:MacProfileSettings.HomeDir .gitconfig -ErrorAction SilentlyContinue)))
+    )
+    process{
+        if(-not ([string]::IsNullOrWhiteSpace($gitconfigpath)) -and (-not (test-path $gitconfigpath))) {
+            Sayed-ConfigureGit
+        }
+        else{
+            'inside else1: [{0}]' -f $gitconfigpath | Write-Error
+        }
+    } 
+}
+
+#### Start script
+
+Set-InitialPath
+if( (Import-MyModules) -eq $true){
+    Ensure-GitConfigExists
 }
