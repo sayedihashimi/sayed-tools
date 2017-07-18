ad403b39015bf6080b592cab058e18738e2f0906
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-07-18 11:37:01 -0700

    removing whitspace

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,7 +1,6 @@
 # run the build script
 $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 
-
 $genTemplatePath = (Join-Path $scriptDir 'powershell\dotnet\gen-template-report.ps1')
 
-& $genTemplatePath -verbose
+& $genTemplatePath
