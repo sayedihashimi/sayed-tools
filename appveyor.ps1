7396e6fbfa04b2cad236c410f1e8db0cfdd98e6d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-11-13 13:25:44 -0800

    changes to automate publish of json report

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,6 +1,6 @@
 # run the build script
 $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 
-$genTemplatePath = (Join-Path $scriptDir 'powershell\dotnet\gen-template-report.ps1')
+$genTemplatePath = (Join-Path $scriptDir 'powershell\dotnet\gen-template-report.ps1' -publishJsonReport)
 
 & $genTemplatePath
--- powershell/dotnet/gen-template-report.ps1
+++ powershell/dotnet/gen-template-report.ps1
@@ -12,7 +12,16 @@ param(
     [string]$newtonsoftDownloadUrl = 'http://www.nuget.org/api/v2/package/Newtonsoft.Json/10.0.2',
 
     [Parameter(Position=3)]
-    [string]$newtonsoftFilename = 'Newtonsoft.Json-10.0.2.nupkg'
+    [string]$newtonsoftFilename = 'Newtonsoft.Json-10.0.2.nupkg',
+
+    [Parameter(Position=4)]
+    [switch]$publishJsonReport,
+
+    [Parameter(Position=5)]
+    [string]$publishUsername = $env:publishUsername,
+
+    [Parameter(Position=6)]
+    [string]$publishPassword = $env:publishPassword
 )
 
 function InternalGet-ScriptDirectory{
@@ -592,6 +601,109 @@ function Save-ToFileAsJson{
     }
 }
 
+function Get-MSDeploy{
+    [cmdletbinding()]
+    param(
+        [string]$msdeployPath = 'C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe'
+    )
+    process{
+        if(test-path $msdeployPath) {
+            Set-Alias -Name msdeploy -Value $msdeployPath -Scope Global
+            return $msdeployPath
+        }
+        else {
+            throw ('msdeploye.exe not found at [{0}]' -f $msdeployPath)
+        }
+    }
+}
+
+function Invoke-CommandString{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
+        [string[]]$command,
+        
+        [Parameter(Position=1)]
+        $commandArgs,
+
+        $ignoreErrors,
+
+        [bool]$maskSecrets,
+
+        [switch]$disableCommandQuoting
+    )
+    process{
+        foreach($cmdToExec in $command){
+            'Executing command [{0}]' -f $cmdToExec | Write-Verbose
+            
+            # write it to a .cmd file
+            $destPath = "$([System.IO.Path]::GetTempFileName()).cmd"
+            if(Test-Path $destPath){Remove-Item $destPath|Out-Null}
+            
+            try{
+                $commandstr = $cmdToExec
+                if(-not $disableCommandQuoting -and $commandstr.Contains(' ') -and (-not ($commandstr -match '''.*''|".*"' ))){
+                    $commandstr = ('"{0}"' -f $commandstr)
+                }
+
+                '{0} {1}' -f $commandstr, ($commandArgs -join ' ') | Set-Content -Path $destPath | Out-Null
+
+                $actualCmd = ('"{0}"' -f $destPath)
+                cmd.exe /D /C $actualCmd
+
+                if(-not $ignoreErrors -and ($LASTEXITCODE -ne 0)){
+                    $msg = ('The command [{0}] exited with code [{1}]' -f $commandstr, $LASTEXITCODE)
+                    throw $msg
+                }
+            }
+            finally{
+                if(Test-Path $destPath){Remove-Item $destPath -ErrorAction SilentlyContinue |Out-Null}
+            }
+        }
+    }
+}
+
+function DeployTemplateReport{
+    [cmdletbinding()]
+    param(
+        [string]$publishUsername = $env:publishUsername,
+        [string]$publishPassword = $env:publishPassword,
+        [string]$deployUrl = 'https://dotnetnew-api.scm.azurewebsites.net/msdeploy.axd?site=dotnetnew-api',
+        [string]$sourceRelFilepath = 'template-report.json',
+        [string]$destRelFilepath = 'wwwroot/template-report.json'
+    )
+    process{
+    # msdeploy -verb:sync -whatif -source:contentPath='C:\data\mycode\sayed-tools\powershell\dotnet\template-report2.json' -dest:contentPath='wwwroot/template-report.json',ComputerName="https://dotnetnew-api.scm.azurewebsites.net/msdeploy.axd?site=dotnetnew-api",UserName='%pubusername%',Password='%pubpwd%',AuthType='Basic'
+
+        if([string]::IsNullOrWhiteSpace($publishUsername)){
+            throw '$publishUsername empty, cannot publish'
+        }
+        if([string]::IsNullOrWhiteSpace($publishPassword)){
+            throw '$publishPassword empty, cannot publish'
+        }
+
+        # check that the source file is on disk
+        [string]$sourceFile = join-path $scriptDir $sourceRelFilepath | Get-FullPathNormalized
+        if(-not (test-path $sourceFile)){
+            throw ('source file not found at [{0}], from relpath: [{1}]' -f $sourceFile, $sourceRelFilepath)
+        }
+
+        # msdeploy -verb:sync -whatif -source:contentPath=''{0}'' -dest:contentPath=''{1}'',ComputerName="{2}",UserName=''{3}'',Password=''{4}'',AuthType=''Basic'' -retryAttempts=10 -retryInterval=2000 
+
+        [string]$msdeployCmdArgs = ('-verb:sync -source:contentPath=''{0}'' -dest:contentPath=''{1}'',ComputerName="{2}",UserName=''{3}'',Password=''{4}'',AuthType=''Basic'' -retryAttempts=10 -retryInterval=2000 ' -f $sourceFile,$destRelFilepath,$deployUrl,$publishUsername,$publishPassword)
+        $logfilepath = "$([System.IO.Path]::GetTempFileName()).log"
+        'Starting publish, logfile={0}' -f $logfilepath | Write-Output
+        try{
+            # wrap the call and grab all output. This is needed to mask any secrets that may appear in the logs
+            Invoke-CommandString -command (Get-MSDeploy) -commandArgs $msdeployCmdArgs *> $logfilepath
+        }
+        catch{
+        }
+        $logcontent = Get-Content $logfilepath
+        $logcontent.Replace($publishUsername,'***USERNAME***').Replace($publishPassword,'***PASSWORD***') | Write-Output
+    }
+}
+
 function Print-Report{
     [cmdletbinding()]
     param(
@@ -712,6 +824,11 @@ try{
     if(-not ($skipReport)){
         & (get-nuget) update -self
         Run-FullReport -searchTerm $searchTerm
+
+        if($publishJsonReport){
+            
+        }
+
     }
 }
 catch{
