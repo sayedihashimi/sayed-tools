ab18912ae486ea134785ccf89174cca55732b7c6
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-12-06 11:31:39 -0800

    adding project

---
+++ aspnet/analyze-rebuild-time.ps1
@@ -0,0 +1,53 @@
+﻿[cmdletbinding()]
+param(
+    [string[]]$projFiles = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+)
+
+function Get-Fullpath{
+    [cmdletbinding()]
+    param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipeline = $true)]
+        $path,
+
+        $workingDir = ($pwd)
+    )
+    process{
+        $fullPath = $path
+        $oldPwd = $pwd
+
+        Push-Location
+        Set-Location $workingDir
+        [Environment]::CurrentDirectory = $pwd
+        $fullPath = ([System.IO.Path]::GetFullPath($path))
+        
+        Pop-Location
+        [Environment]::CurrentDirectory = $oldPwd
+
+        return $fullPath
+    }
+}
+
+function GetBuildTime{
+    [cmdletbinding()]
+    param(
+        [string[]]$projFile
+    )
+    process{
+        foreach($proj in $projFile){
+            $filepath = (Get-fullpath -path $proj)
+            $timetaken = Measure-Command -Expression { &"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" $filepath --% /m /clp:v=m;Summary }
+            
+            @{
+                'ProjectFile' = $filepath
+                'Build (seconds)' = $timetaken
+            }
+        }
+        #$timetaken = Measure-Command -Expression { &"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" --% "C:\Users\sayedha\source\repos\WebApplication20\WebApplication20\WebApplication20.csproj" /m /clp:v=m;Summary }
+        #$timetaken.TotalSeconds
+    }
+}
+
+
+GetBuildTime -projFile $projFiles
---
+++ aspnet/rebuild/src/Wap01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wap01.MvcApplication" Language="C#" %>
